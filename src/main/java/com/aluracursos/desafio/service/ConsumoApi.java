cde00fa8dc6b1288ed3043ca59c6a59466480364
gonzalo araya <gonzalo araya>
2024-07-12 20:59:12 -0300

    consumo de api

---
+++ src/main/java/com/aluracursos/desafio/service/ConsumoApi.java
@@ -0,0 +1,29 @@
+package com.aluracursos.desafio.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoApi {
+    public String obtenerDatos(String url) {
+        String json;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = null;
+
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        json = response.body();
+        return json;
+    }
+}
